ab851ca082be81923fb333b5051d48d72beb9271
Luhao Yao <Luhao.Yao@partner.bmwgroup.com>
2020-09-11 16:48:55 +0800

    update the real result to 2019

--- db/real_result.py
+++ db/real_result.py
@@ -430,28 +430,328 @@ collection.insert_one({
     "winner_ratio": 2148.01,
     "detail_data": [
         {"magnification": 1, "count": (369142, 161), "percentage": (
-            0.1259, 0.0248), "winning_percent": (0.0005, 0.0004)},
+            0.1259, 0.0254), "winning_percent": (0.0005, 0.0004)},
         {"magnification": 2, "count": (371715, 353), "percentage": (
-            0.1268, 0.0513), "winning_percent": (0.0009, 0.0009)},
+            0.1268, 0.0557), "winning_percent": (0.0009, 0.0009)},
         {"magnification": 3, "count": (407283, 546), "percentage": (
-            0.1389, 0.0985), "winning_percent": (0.0014, 0.0016)},
+            0.1389, 0.0862), "winning_percent": (0.0014, 0.0013)},
         {"magnification": 4, "count": (416415, 724), "percentage": (
-            0.1420, 0.1333), "winning_percent": (0.0019, 0.0021)},
+            0.1420, 0.1143), "winning_percent": (0.0019, 0.0017)},
         {"magnification": 5, "count": (357288, 830), "percentage": (
-            0.1218, 0.1314), "winning_percent": (0.0023, 0.0025)},
-        {"magnification": 6, "count": (249747, 757), "percentage": (
-            0.0852, 0.1132), "winning_percent": (0.0028, 0.0030)},
+            0.1218, 0.1311), "winning_percent": (0.0023, 0.0023)},
+        {"magnification": 6, "count": (249747, 751), "percentage": (
+            0.0852, 0.1186), "winning_percent": (0.0028, 0.0030)},
         {"magnification": 7, "count": (263758, 835), "percentage": (
-            0.0900, 0.1356), "winning_percent": (0.0033, 0.0033)},
+            0.0900, 0.1318), "winning_percent": (0.0033, 0.0032)},
         {"magnification": 8, "count": (208610, 789), "percentage": (
-            0.0711, 0.1164), "winning_percent": (0.0037, 0.0039)},
-        {"magnification": 9, "count": (133910, 577), "percentage": (
-            0.0457, 0.0911), "winning_percent": (0.0042, 0.0046)},
-        {"magnification": 10, "count": (100011, 458), "percentage": (
-            0.0341, 0.0723), "winning_percent": (0.0047, 0.0047)},
-        {"magnification": 11, "count": (54219, 203), "percentage": (
-            0.0185, 0.0321), "winning_percent": (0.0051, 0.0053)},
+            0.0711, 0.1246), "winning_percent": (0.0037, 0.0038)},
+        {"magnification": 9, "count": (133910, 600), "percentage": (
+            0.0457, 0.0947), "winning_percent": (0.0042, 0.0045)},
+        {"magnification": 10, "count": (100011, 450), "percentage": (
+            0.0341, 0.0711), "winning_percent": (0.0047, 0.0045)},
+        {"magnification": 11, "count": (54319, 294), "percentage": (
+            0.0185, 0.0464), "winning_percent": (0.0051, 0.0054)},
         {"magnification": 12, "count": (29, 0), "percentage": (
             0.0000, 0.0000), "winning_percent": (0.0056, 0.0000)},
+    ]
+})
+
+collection.insert_one({
+    "lottery_time": "2018/10/26",
+    "a_code": 3_003_201,
+    "generation_time": "2018/10/26 10:15",
+    "total_ids": 14_123_942,
+    "winners": 6402,
+    "winning_percent": 0.000453,
+    "winner_ratio": 2206.18,
+    "detail_data": [
+        {"magnification": 1, "count": (372571, 161), "percentage": (
+            0.1241, 0.0283), "winning_percent": (0.0005, 0.0005)},
+        {"magnification": 2, "count": (374140, 331), "percentage": (
+            0.1268, 0.0517), "winning_percent": (0.0009, 0.0009)},
+        {"magnification": 3, "count": (403137, 548), "percentage": (
+            0.1342, 0.0856), "winning_percent": (0.0014, 0.0014)},
+        {"magnification": 4, "count": (419793, 760), "percentage": (
+            0.1398, 0.1187), "winning_percent": (0.0018, 0.0018)},
+        {"magnification": 5, "count": (377295, 845), "percentage": (
+            0.1256, 0.1320), "winning_percent": (0.0023, 0.0022)},
+        {"magnification": 6, "count": (258198, 678), "percentage": (
+            0.0860, 0.1059), "winning_percent": (0.0027, 0.0026)},
+        {"magnification": 7, "count": (260805, 868), "percentage": (
+            0.0868, 0.1356), "winning_percent": (0.0032, 0.0033)},
+        {"magnification": 8, "count": (225100, 774), "percentage": (
+            0.0750, 0.1209), "winning_percent": (0.0036, 0.0034)},
+        {"magnification": 9, "count": (139241, 587), "percentage": (
+            0.0464, 0.0917), "winning_percent": (0.0041, 0.0042)},
+        {"magnification": 10, "count": (102927, 488), "percentage": (
+            0.0343, 0.0762), "winning_percent": (0.0045, 0.0047)},
+        {"magnification": 11, "count": (69957, 341), "percentage": (
+            0.0233, 0.0533), "winning_percent": (0.0050, 0.0049)},
+        {"magnification": 12, "count": (37, 1), "percentage": (
+            0.0000, 0.0002), "winning_percent": (0.0054, 0.0200)},
+    ]
+})
+
+collection.insert_one({
+    "lottery_time": "2018/12/26",
+    "a_code": 3_060_913,
+    "generation_time": "2018/12/26 10:15",
+    "total_ids": 14_620_617,
+    "winners": 6413,
+    "winning_percent": 0.000439,
+    "winner_ratio": 2279.84,
+    "detail_data": [
+        {"magnification": 1, "count": (368604, 159), "percentage": (
+            0.1204, 0.0247), "winning_percent": (0.0004, 0.0004)},
+        {"magnification": 2, "count": (378516, 335), "percentage": (
+            0.1236, 0.0522), "winning_percent": (0.0008, 0.0008)},
+        {"magnification": 3, "count": (395895, 494), "percentage": (
+            0.1293, 0.0770), "winning_percent": (0.0013, 0.0012)},
+        {"magnification": 4, "count": (421280, 753), "percentage": (
+            0.1376, 0.1174), "winning_percent": (0.0017, 0.0017)},
+        {"magnification": 5, "count": (390309, 876), "percentage": (
+            0.1275, 0.1366), "winning_percent": (0.0021, 0.0022)},
+        {"magnification": 6, "count": (270565, 692), "percentage": (
+            0.0883, 0.1079), "winning_percent": (0.0026, 0.0025)},
+        {"magnification": 7, "count": (254430, 760), "percentage": (
+            0.0831, 0.1185), "winning_percent": (0.0030, 0.0029)},
+        {"magnification": 8, "count": (242707, 854), "percentage": (
+            0.0792, 0.1331), "winning_percent": (0.0035, 0.0035)},
+        {"magnification": 9, "count": (146739, 583), "percentage": (
+            0.0479, 0.0909), "winning_percent": (0.0039, 0.0039)},
+        {"magnification": 10, "count": (106670, 468), "percentage": (
+            0.0348, 0.0729), "winning_percent": (0.0043, 0.0043)},
+        {"magnification": 11, "count": (85152, 439), "percentage": (
+            0.0278, 0.0684), "winning_percent": (0.0048, 0.0051)},
+        {"magnification": 12, "count": (46, 0), "percentage": (
+            0.0002, 0.0000), "winning_percent": (0.0052, 0.0000)},
+    ]
+})
+
+collection.insert_one({
+    "lottery_time": "2019/02/26",
+    "a_code": 3_135_548,
+    "generation_time": "2019/02/26 10:15",
+    "total_ids": 15_175_162,
+    "winners": 6412,
+    "winning_percent": 0.000423,
+    "winner_ratio": 2366.68,
+    "detail_data": [
+        {"magnification": 1, "count": (366313, 134), "percentage": (
+            0.1168, 0.0209), "winning_percent": (0.0004, 0.0004)},
+        {"magnification": 2, "count": (393233, 335), "percentage": (
+            0.1254, 0.0522), "winning_percent": (0.0008, 0.0009)},
+        {"magnification": 3, "count": (392314, 512), "percentage": (
+            0.1251, 0.0799), "winning_percent": (0.0013, 0.0013)},
+        {"magnification": 4, "count": (420923, 744), "percentage": (
+            0.1342, 0.1160), "winning_percent": (0.0017, 0.0018)},
+        {"magnification": 5, "count": (401460, 831), "percentage": (
+            0.1280, 0.1296), "winning_percent": (0.0021, 0.0021)},
+        {"magnification": 6, "count": (289818, 750), "percentage": (
+            0.0924, 0.1170), "winning_percent": (0.0025, 0.0026)},
+        {"magnification": 7, "count": (246670, 710), "percentage": (
+            0.0787, 0.1107), "winning_percent": (0.0030, 0.0029)},
+        {"magnification": 8, "count": (254959, 868), "percentage": (
+            0.0813, 0.1354), "winning_percent": (0.0034, 0.0034)},
+        {"magnification": 9, "count": (158392, 638), "percentage": (
+            0.0505, 0.0995), "winning_percent": (0.0038, 0.0040)},
+        {"magnification": 10, "count": (111652, 438), "percentage": (
+            0.0356, 0.0683), "winning_percent": (0.0042, 0.0039)},
+        {"magnification": 11, "count": (90643, 404), "percentage": (
+            0.0289, 0.0630), "winning_percent": (0.0046, 0.0045)},
+        {"magnification": 12, "count": (9165, 48), "percentage": (
+            0.0029, 0.0075), "winning_percent": (0.0051, 0.0052)},
+        {"magnification": 13, "count": (6, 0), "percentage": (
+            0.0000, 0.0000), "winning_percent": (0.0055, 0.0000)},
+    ]
+})
+
+collection.insert_one({
+    "lottery_time": "2019/04/26",
+    "a_code": 3_209_304,
+    "generation_time": "2019/04/26 10:15",
+    "total_ids": 15_707_277,
+    "winners": 6379,
+    "winning_percent": 0.000406,
+    "winner_ratio": 2462.34,
+    "detail_data": [
+        {"magnification": 1, "count": (377533, 166), "percentage": (
+            0.1176, 0.0260), "winning_percent": (0.0004, 0.0004)},
+        {"magnification": 2, "count": (398433, 336), "percentage": (
+            0.1241, 0.0527), "winning_percent": (0.0008, 0.0008)},
+        {"magnification": 3, "count": (388518, 481), "percentage": (
+            0.1211, 0.0754), "winning_percent": (0.0012, 0.0012)},
+        {"magnification": 4, "count": (417118, 684), "percentage": (
+            0.1300, 0.1072), "winning_percent": (0.0016, 0.0016)},
+        {"magnification": 5, "count": (406237, 803), "percentage": (
+            0.1266, 0.1259), "winning_percent": (0.0020, 0.0020)},
+        {"magnification": 6, "count": (316003, 757), "percentage": (
+            0.0985, 0.1187), "winning_percent": (0.0024, 0.0024)},
+        {"magnification": 7, "count": (242635, 695), "percentage": (
+            0.0756, 0.1090), "winning_percent": (0.0028, 0.0029)},
+        {"magnification": 8, "count": (260527, 818), "percentage": (
+            0.0812, 0.1282), "winning_percent": (0.0032, 0.0031)},
+        {"magnification": 9, "count": (170683, 646), "percentage": (
+            0.0532, 0.1013), "winning_percent": (0.0037, 0.0038)},
+        {"magnification": 10, "count": (116405, 480), "percentage": (
+            0.0363, 0.0752), "winning_percent": (0.0041, 0.0041)},
+        {"magnification": 11, "count": (93768, 403), "percentage": (
+            0.0292, 0.0632), "winning_percent": (0.0045, 0.0043)},
+        {"magnification": 12, "count": (21429, 110), "percentage": (
+            0.0067, 0.0172), "winning_percent": (0.0049, 0.0051)},
+        {"magnification": 13, "count": (15, 0), "percentage": (
+            0.0000, 0.0000), "winning_percent": (0.0053, 0.0000)},
+    ]
+})
+
+collection.insert_one({
+    "lottery_time": "2019/06/26",
+    "a_code": 3_264_065,
+    "generation_time": "2019/06/26 10:15",
+    "total_ids": 16_225_458,
+    "winners": 6373,
+    "winning_percent": 0.000393,
+    "winner_ratio": 2545.97,
+    "detail_data": [
+        {"magnification": 1, "count": (382451, 159), "percentage": (
+            0.1172, 0.0249), "winning_percent": (0.0004, 0.0004)},
+        {"magnification": 2, "count": (392398, 304), "percentage": (
+            0.1202, 0.0477), "winning_percent": (0.0008, 0.0008)},
+        {"magnification": 3, "count": (384872, 473), "percentage": (
+            0.1179, 0.0742), "winning_percent": (0.0012, 0.0012)},
+        {"magnification": 4, "count": (413265, 650), "percentage": (
+            0.1266, 0.1020), "winning_percent": (0.0016, 0.0016)},
+        {"magnification": 5, "count": (409200, 739), "percentage": (
+            0.1254, 0.1160), "winning_percent": (0.0020, 0.0018)},
+        {"magnification": 6, "count": (336103, 754), "percentage": (
+            0.1030, 0.1183), "winning_percent": (0.0024, 0.0022)},
+        {"magnification": 7, "count": (242778, 720), "percentage": (
+            0.0744, 0.1133), "winning_percent": (0.0027, 0.0030)},
+        {"magnification": 8, "count": (260904, 800), "percentage": (
+            0.0799, 0.1255), "winning_percent": (0.0031, 0.0031)},
+        {"magnification": 9, "count": (187399, 677), "percentage": (
+            0.0574, 0.1062), "winning_percent": (0.0035, 0.0036)},
+        {"magnification": 10, "count": (123138, 502), "percentage": (
+            0.0377, 0.0788), "winning_percent": (0.0039, 0.0041)},
+        {"magnification": 11, "count": (95438, 421), "percentage": (
+            0.0292, 0.0661), "winning_percent": (0.0043, 0.0044)},
+        {"magnification": 12, "count": (36097, 172), "percentage": (
+            0.0111, 0.0270), "winning_percent": (0.0047, 0.0048)},
+        {"magnification": 13, "count": (22, 0), "percentage": (
+            0.0000, 0.0000), "winning_percent": (0.0051, 0.0000)},
+    ]
+})
+
+collection.insert_one({
+    "lottery_time": "2019/08/26",
+    "a_code": 3_317_404,
+    "generation_time": "2019/08/26 10:15",
+    "total_ids": 16_750_880,
+    "winners": 6389,
+    "winning_percent": 0.000381,
+    "winner_ratio": 2621.83,
+    "detail_data": [
+        {"magnification": 1, "count": (381766, 126), "percentage": (
+            0.1151, 0.0197), "winning_percent": (0.0004, 0.0003)},
+        {"magnification": 2, "count": (390338, 306), "percentage": (
+            0.1177, 0.0479), "winning_percent": (0.0008, 0.0008)},
+        {"magnification": 3, "count": (381175, 431), "percentage": (
+            0.1149, 0.0675), "winning_percent": (0.0011, 0.0011)},
+        {"magnification": 4, "count": (409003, 616), "percentage": (
+            0.1233, 0.0964), "winning_percent": (0.0015, 0.0015)},
+        {"magnification": 5, "count": (413526, 774), "percentage": (
+            0.1247, 0.1211), "winning_percent": (0.0019, 0.0019)},
+        {"magnification": 6, "count": (352525, 841), "percentage": (
+            0.1063, 0.1316), "winning_percent": (0.0023, 0.0024)},
+        {"magnification": 7, "count": (247139, 682), "percentage": (
+            0.0745, 0.1067), "winning_percent": (0.0027, 0.0028)},
+        {"magnification": 8, "count": (259349, 762), "percentage": (
+            0.0782, 0.1193), "winning_percent": (0.0031, 0.0029)},
+        {"magnification": 9, "count": (203933, 692), "percentage": (
+            0.0615, 0.1083), "winning_percent": (0.0034, 0.0034)},
+        {"magnification": 10, "count": (130648, 509), "percentage": (
+            0.0394, 0.0797), "winning_percent": (0.0038, 0.0039)},
+        {"magnification": 11, "count": (96500, 405), "percentage": (
+            0.0291, 0.0634), "winning_percent": (0.0042, 0.0042)},
+        {"magnification": 12, "count": (51472, 245), "percentage": (
+            0.0155, 0.0383), "winning_percent": (0.0046, 0.0048)},
+        {"magnification": 13, "count": (29, 0), "percentage": (
+            0.0000, 0.0000), "winning_percent": (0.0050, 0.0000)},
+    ]
+})
+
+collection.insert_one({
+    "lottery_time": "2019/10/26",
+    "a_code": 3_317_568,
+    "generation_time": "2019/10/26 10:15",
+    "total_ids": 17_099_033,
+    "winners": 6383,
+    "winning_percent": 0.000373,
+    "winner_ratio": 2678.84,
+    "detail_data": [
+        {"magnification": 1, "count": (371189, 129), "percentage": (
+            0.1119, 0.0202), "winning_percent": (0.0004, 0.0003)},
+        {"magnification": 2, "count": (375188, 289), "percentage": (
+            0.1131, 0.0453), "winning_percent": (0.0007, 0.0008)},
+        {"magnification": 3, "count": (369094, 403), "percentage": (
+            0.1113, 0.0631), "winning_percent": (0.0011, 0.0011)},
+        {"magnification": 4, "count": (394829, 577), "percentage": (
+            0.1190, 0.0904), "winning_percent": (0.0015, 0.0015)},
+        {"magnification": 5, "count": (410636, 750), "percentage": (
+            0.1238, 0.1117), "winning_percent": (0.0019, 0.0018)},
+        {"magnification": 6, "count": (368304, 877), "percentage": (
+            0.1110, 0.1374), "winning_percent": (0.0022, 0.0024)},
+        {"magnification": 7, "count": (252637, 652), "percentage": (
+            0.0762, 0.1021), "winning_percent": (0.0026, 0.0028)},
+        {"magnification": 8, "count": (255248, 765), "percentage": (
+            0.0769, 0.1198), "winning_percent": (0.0030, 0.0030)},
+        {"magnification": 9, "count": (219395, 767), "percentage": (
+            0.0661, 0.1202), "winning_percent": (0.0034, 0.0035)},
+        {"magnification": 10, "count": (135317, 488), "percentage": (
+            0.0408, 0.0765), "winning_percent": (0.0037, 0.0036)},
+        {"magnification": 11, "count": (99110, 388), "percentage": (
+            0.0299, 0.0608), "winning_percent": (0.0041, 0.0039)},
+        {"magnification": 12, "count": (66585, 298), "percentage": (
+            0.0201, 0.0467), "winning_percent": (0.0045, 0.0045)},
+        {"magnification": 13, "count": (36, 0), "percentage": (
+            0.0000, 0.0000), "winning_percent": (0.0049, 0.0000)},
+    ]
+})
+
+collection.insert_one({
+    "lottery_time": "2019/12/26",
+    "a_code": 3_335_437,
+    "generation_time": "2019/12/26 10:15",
+    "total_ids": 17_493_388,
+    "winners": 6384,
+    "winning_percent": 0.000365,
+    "winner_ratio": 2740.19,
+    "detail_data": [
+        {"magnification": 1, "count": (372312, 107), "percentage": (
+            0.1116, 0.0168), "winning_percent": (0.0004, 0.0003)},
+        {"magnification": 2, "count": (359586, 244), "percentage": (
+            0.1078, 0.0382), "winning_percent": (0.0007, 0.0007)},
+        {"magnification": 3, "count": (362343, 391), "percentage": (
+            0.1086, 0.0612), "winning_percent": (0.0011, 0.0011)},
+        {"magnification": 4, "count": (381066, 585), "percentage": (
+            0.1142, 0.0916), "winning_percent": (0.0015, 0.0015)},
+        {"magnification": 5, "count": (407661, 746), "percentage": (
+            0.1222, 0.1169), "winning_percent": (0.0018, 0.0018)},
+        {"magnification": 6, "count": (378509, 829), "percentage": (
+            0.1135, 0.1299), "winning_percent": (0.0022, 0.0022)},
+        {"magnification": 7, "count": (263499, 677), "percentage": (
+            0.0790, 0.1060), "winning_percent": (0.0026, 0.0026)},
+        {"magnification": 8, "count": (248556, 712), "percentage": (
+            0.0745, 0.1115), "winning_percent": (0.0029, 0.0029)},
+        {"magnification": 9, "count": (235860, 789), "percentage": (
+            0.0707, 0.1236), "winning_percent": (0.0033, 0.0033)},
+        {"magnification": 10, "count": (142112, 530), "percentage": (
+            0.0426, 0.0830), "winning_percent": (0.0036, 0.0037)},
+        {"magnification": 11, "count": (102792, 436), "percentage": (
+            0.0308, 0.0683), "winning_percent": (0.0040, 0.0042)},
+        {"magnification": 12, "count": (81094, 338), "percentage": (
+            0.0243, 0.0529), "winning_percent": (0.0044, 0.0042)},
+        {"magnification": 13, "count": (47, 0), "percentage": (
+            0.0000, 0.0000), "winning_percent": (0.0047, 0.0000)},
     ]
 })
